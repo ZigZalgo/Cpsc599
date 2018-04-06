1c8cfd694f2db6a2a7f5ceb5294c0974795d4b30
nathan.douglas <nathan.douglas@ucalgary.ca>
2018-04-05 22:00:11 -0600

    Implemented Or-Tree search method

--- C45NCDB/DecisionTree/C4p5.cs
+++ C45NCDB/DecisionTree/C4p5.cs
@@ -22,6 +22,13 @@ namespace C45NCDB.DecisionTree
         public static int MinDivSize { get; set; } = 100;
         public static bool predict = false;
 
+        //more static fields for -o.
+        //I think these can be non-static, but following what's already here.
+        //TODO: optimize default values.
+        public static int MaxBreadth { get; set; } = 5;
+        public static double MinimumInformationGain { get; set; } = 0.1;
+        private List<CollisionEntry> allEntries;
+
 
         public static void SetHeaderToPredict(string header)
         {
@@ -40,6 +47,9 @@ namespace C45NCDB.DecisionTree
             nextIterationNodes = new List<Node>();
             unusedRules = Pre_Generated_Rules;
             currentNodes.Add(Root);
+
+            //need for InfoGain calculation
+            allEntries = collisions;
         }
 
         public void Learn()
@@ -68,6 +78,23 @@ namespace C45NCDB.DecisionTree
             Console.WriteLine(currentNodes.Count + " Number of unlearned nodes");
             nextIterationNodes = new List<Node>();
             Learn();
+        }
+
+        public void LearnOrTree(Node node)
+        {
+            //generally the top of the recursion. Applys all the unused rules to the node,
+            //ignoring data which doesn't match the rule list
+            if (unusedRules.Count > 0)
+            {
+                node.ApplyAllRules(unusedRules);
+            }
+            //main recursive case, generate rules from coverage finds the MaxBreadth best rules
+            //for each rule, recur into the node created by applying that rule to the current node's collision set.
+            List<Rule> bestRules = Helper.GenerateRulesFromCoverage(node.currentEntries, allEntries);
+            foreach (Rule rule in bestRules)
+            {
+                LearnOrTree(node.ChildByRule(rule));
+            }
         }
 
         public void PrintRules(string FilePath)
@@ -169,6 +196,32 @@ namespace C45NCDB.DecisionTree
             Right = new Node(passed, depth + 1, passedRules, this);
             nextIterationNodes.Add(Left);
             nextIterationNodes.Add(Right);
+        }
+
+        /**
+         * Creates a child rule to this node by applying the rule to its current entries
+         * Open to method name suggestions.
+         * Still tempted to call it reproduce
+         */
+        internal Node ChildByRule(Rule rule)
+        {
+            List<CollisionEntry> passed = new List<CollisionEntry>();
+            List<CollisionEntry> failed = new List<CollisionEntry>();
+            CollisionEntry.EvaluateEntries(currentEntries, failed, passed, rule);
+
+            List<Rule> childRules = new List<Rule>(usedRules) { rule };
+            return new Node(passed, depth + 1, childRules, this);
+        }
+
+        internal void ApplyAllRules(List<Rule> rules)
+        {
+            List<CollisionEntry> passed = new List<CollisionEntry>();
+            List<CollisionEntry> failed = new List<CollisionEntry>();
+            foreach (Rule rule in rules)
+            {
+                CollisionEntry.EvaluateEntries(currentEntries, failed, passed, rule);
+                currentEntries = passed;
+            }
         }
 
         public void PrintRules(StreamWriter writer)
--- C45NCDB/Tools/Helper.cs
+++ C45NCDB/Tools/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using C45NCDB.RuleEngine;
+using C45NCDB.DecisionTree;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -61,6 +62,95 @@ namespace C45NCDB.Tools
         {
             if (header_To_Predict == -1)
                 throw new Exception("You forgot to se the Header to predict");
+            return GenerateRuleFromEvenDivisions(currentEntries);
+        }
+
+        public static List<Rule> GenerateRulesFromCoverage(List<CollisionEntry> currentNodeCollection, List<CollisionEntry> total)
+        {
+            //double information_gain_best = double.MinValue;
+            //RuleInfo[] informationGainBest = new RuleInfo[C4p5.MaxBreadth];
+            List<RuleInfo> informationGainBest = new List<RuleInfo>();
+
+            for (int i = 0; i < CollisionEntry.headers.Length; i++)
+            { 
+                //Tally values into the dictionary
+                Dictionary<int, int> count = new Dictionary<int, int>();
+                foreach (CollisionEntry c in currentNodeCollection)
+                {
+                    if (count.ContainsKey(c.vals[i]))
+                        count[c.vals[i]]++;
+                    else
+                    {
+                        count.Add(c.vals[i], 0);
+                    }
+                }
+                //get P(maxKey)
+                int maxKey = count.FirstOrDefault(x => x.Value == count.Values.Max()).Key;
+                double maxVal = (double)count[maxKey] / (double)currentNodeCollection.Count;
+
+                //tally total occurances of maxkey in the whole db
+                int maxKeyTotalOccurances = 0;
+                foreach (CollisionEntry c in total)
+                {
+                    if (c.vals[i] == count[maxKey]) maxKeyTotalOccurances++;
+                }
+
+                //calculate information gain.
+                /*
+                 * Gonna use as close to what exists in the slides as possible
+                 * IG = H(A) - H(A|B)
+                 * where 
+                 *  A = the condition under consideration
+                 *  B = All previous rules
+                 *  H(A) = the number of entries that match A out of all entries
+                 *  H(A|B) = the number of entries that match B and then A
+                 */
+                double infoGain = (maxKeyTotalOccurances / total.Count) - maxVal;
+                if (informationGainBest.Count < C4p5.MaxBreadth)
+                {
+                    //add it to the list if not enough good rules have yet been found
+                    informationGainBest.Add(new RuleInfo(infoGain, i, maxKey));
+                }
+                else if (infoGain > informationGainBest[C4p5.MaxBreadth].informationGain)
+                {
+                    //pop off the least best and replace it with this new better value
+                    //Then sort all the most best to ensure list integrity
+                    informationGainBest[C4p5.MaxBreadth - 1] = new RuleInfo(infoGain, i, maxKey);
+                    informationGainBest.Sort();
+                }
+            }
+
+            //Rule r = new Rule(header_corresponding, value_corresponding, HValType.HeaderCompVal, Operator.Equals);
+            List<Rule> rules = new List<Rule>();
+            foreach (RuleInfo ruleInfo in informationGainBest)
+            {
+                rules.Add(ruleInfo.MakeRule());
+            }
+            return rules;
+        }
+    }
+
+    public class RuleInfo : IComparable<RuleInfo>
+    {
+        public double informationGain;
+        public int headerCorresponding;
+        public int valueCorresponding;
+
+        public RuleInfo(double informationGain, int headerCorresponding, int valueCorresponding)
+        {
+            this.informationGain = informationGain;
+            this.headerCorresponding = headerCorresponding;
+            this.valueCorresponding = valueCorresponding;
+        }
+
+        public Rule MakeRule()
+        {
+            return new Rule(headerCorresponding, valueCorresponding, HValType.HeaderCompVal, Operator.Equals);
+        }
+
+        public int CompareTo(RuleInfo other)
+        {
+            return informationGain.CompareTo(other.informationGain);
         }
     }
 }

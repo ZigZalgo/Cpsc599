a6adde6ce487edf4c3548f5e6749ff6b21e1d90b
Ben Roberts <benjamin.roberts@ucalgary.ca>
2018-04-09 13:00:22 -0600

    Added full explanation of command line args

--- C45NCDB/Program.cs
+++ C45NCDB/Program.cs
@@ -13,9 +13,16 @@ namespace C45NCDB
     {
         public static void Main(string[] args)
         {
-			if (args.Length < 4) {
-				Console.WriteLine("Incorrect number of arguments:");
-				Console.WriteLine("<Binary File> <Rules File> <Output File> <Header Config File> [Header to Predict]");
+			if (args.Length == 0 || args[0].Equals("help") || args[0].Equals("--help") || args[0].Equals("-h") || args.Length < 4) {
+				Console.WriteLine("Args: <Binary File> <Rules File> <Output File> <Header Config File> [Header to Predict]\n");
+				Console.WriteLine("<Binary File> - Pregenerated Binary file of the data to build the tree with\n");
+				Console.WriteLine("<Rules File> - Rules to build the tree with before running the C4.5 algorithm.\n" +
+					"\tOne rule per line. Ex. C_HOUR (==,>,<) X");
+				Console.WriteLine("<Output File> - File where all of the generated rules and final statistics will be output\n");
+				Console.WriteLine("<Header Config File> - File where headers can be configured. \n\tYou can either tell the algorithm to" +
+					" ignore a header or mark a header as continuous. \n\tOne config per line. Ex. (ignore,cont) C_HOUR\n");
+				Console.WriteLine("[Header to Predict] - One of the headers. Ex. C_HOUR. \n\tOptional, but required if you want to run the " +
+					"C4.5 algorithm. \n\tIf not specified, then a binary tree will be created.\n");
 			} else {
 				CollisionEntry[] collisions = CollisionEntry.ReadCollisionsFromFile(args[0]);
 				RulesGenerator gen = new RulesGenerator(args[1]);
